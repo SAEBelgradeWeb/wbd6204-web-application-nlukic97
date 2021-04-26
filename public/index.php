eb0d015d001d57c77d8d45923321035ff2d3f33c
nlukic97 <nlukic97@gmail.com>
2021-04-26 08:35:04 +0200

    testing the cors policy change

--- public/index.php
+++ public/index.php
@@ -46,6 +46,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
