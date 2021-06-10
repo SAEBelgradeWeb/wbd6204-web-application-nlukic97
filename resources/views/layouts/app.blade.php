01e4b61fe7642129dfcbb36f857e041a712fbc67
nlukic97 <nlukic97@gmail.com>
2021-06-10 17:45:16 +0200

    added google analytics tracking tag to the app.blade.php page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8F55HVMQCC"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-8F55HVMQCC');
+    </script>
+
+
 </head>
 <body>
     <div id="app">
