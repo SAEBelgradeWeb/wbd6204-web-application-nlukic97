6fe0fbbf2d168458f9c8897e9aa33ef4831d42a2
nlukic97 <nlukic97@gmail.com>
2021-06-10 17:49:40 +0200

    sorting out website.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,15 +22,6 @@
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-8F55HVMQCC"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-8F55HVMQCC');
-    </script>
-
-
 </head>
 <body>
     <div id="app">
